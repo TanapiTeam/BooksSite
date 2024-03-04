afd4a378bf7c15f11cffd57f1b3b0ab038ee4805
shunnadamoto <shun1829@gmail.com>
2024-03-04 19:18:28 +0900

    コンフリクト解消

---
+++ src/components/Language/Language.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import st from './index.module.scss'
+
+const LanguageList = { id: '1', language: 'Python', context: <div></div> }
+
+export const Language: FC = () => {
+  return (
+    <div className={st.flex}>
+      <strong>人気のトピック</strong>
+      <div className={st.LanguageList}>
+        <div>
+          <img
+            src='https://storage.googleapis.com/zenn-user-upload/topics/ebddf7c6bb.png'
+            className={st.LanguageList__icon}
+          />
+        </div>
+        <div>{LanguageList.language}</div>
+      </div>
+      <div className={st.LanguageList}>
+        <div>
+          <img
+            src='https://storage.googleapis.com/zenn-user-upload/topics/ebddf7c6bb.png'
+            className={st.LanguageList__icon}
+          />
+        </div>
+        <div>{LanguageList.language}</div>
+      </div>
+    </div>
+  )
+}
