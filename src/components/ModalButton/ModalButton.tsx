8bd4253f68766b11861e524682df0a61a0a1baed
hirokimatsuoka <celica24ss3@gmail.com>
2024-03-08 11:52:36 +0900

    モーダルコンポーネントのcss調整

--- src/components/ModalButton/ModalButton.tsx
+++ src/components/ModalButton/ModalButton.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useState } from 'react';
+import { FC, ReactNode } from 'react';
 import Modal from 'react-modal';
 import st from './index.module.scss';
 import Link from 'next/link';
@@ -10,40 +10,37 @@ type Props = {
 };
 
 export const ModalButton: FC<Props> = ({ isOpen, closeModal }) => {
-    const [modalContent, setModalContent] = useState<string>('');
-    const openModal = () => {
-        setModalContent('モーダルの内容');
-    };
-
     return (
         <div>
             <Modal isOpen={isOpen} onRequestClose={closeModal} contentLabel='Example Modal'>
                 <div className={st.container}>
                     <h2 className={st.container__title}>本のタイトル</h2>
-                    <p>{modalContent}</p>
-                    <div className={st.container__imgBox}>
-                        <img
-                            src='https://picsum.photos/200'
-                            alt='本の画像'
-                            className={st.container__thumbnail}
-                        />
-                    </div>
-                    <div>
-                        <div className={st.container__bookInfo}>
-                            <p>著者:</p>
-                            <p></p>
+
+                    <div className={st.container__bookInfo}>
+                        <div className={st.container__imgBox}>
+                            <img
+                                src='https://picsum.photos/500'
+                                alt='本の画像'
+                                className={st.container__thumbnail}
+                            />
                         </div>
-                        <div className={st.container__bookInfo}>
-                            <p className={st.container__}>ページ数:</p>
-                            <p></p>
-                        </div>
-                        <div className={st.container__bookInfo}>
-                            <p className={st.container__list}>発売日:</p>
-                            <p></p>
-                        </div>
-                        <div className={st.container__bookContent}>
-                            <p className={st.container__list}>概要:</p>
-                            <div></div>
+                        <div className={st.container__bookIntro}>
+                            <div className={st.container__bookContent}>
+                                <p className={st.container__bookContent__author}>著者:</p>
+                                <p></p>
+                            </div>
+                            <div className={st.container__bookContent}>
+                                <p className={st.container__bookContent__pageNumber}>ページ数:</p>
+                                <p></p>
+                            </div>
+                            <div className={st.container__bookContent}>
+                                <p className={st.container__bookContent__pubulshDate}>発売日:</p>
+                                <p></p>
+                            </div>
+                            <div className={st.container__bookTextArea}>
+                                <p className={st.container__bookTextArea__overview}>概要:</p>
+                                <div></div>
+                            </div>
                         </div>
                     </div>
                     <div className={st.container__linkBox}>
